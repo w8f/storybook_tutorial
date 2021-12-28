9e98094d0151750716977c6d868b97a9a8c5f5c8
w8f <haya22aws@gmail.com>
2021-12-28 17:02:12 +0900

    複合的なコンポーネントのストーリー作成

---
+++ front/src/components/TaskList/TaskList.js
@@ -0,0 +1,72 @@
+import Task from "../Task/Task";
+import PropTypes from "prop-types";
+
+const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
+  if (loading) {
+    return (
+      <div className="list-items">
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <div className="title-message">You have no tasks.</div>
+          <div className="subtitle-message">Sit back and relax.</div>
+        </div>
+      </div>
+    );
+  }
+
+  const tasksInOrder = [
+    ...tasks.filter((task) => task.state === "TASK_PINNED"),
+    ...tasks.filter((task) => task.state !== "TASK_PINNED"),
+  ];
+
+  return (
+    <div>
+      {tasksInOrder.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+};
+
+TaskList.propTypes = {
+  /** Checks if it's in loading state */
+  loading: PropTypes.bool,
+  /** The list of tasks */
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+};
+TaskList.defaultProps = {
+  loading: false,
+};
+
+export default TaskList;
